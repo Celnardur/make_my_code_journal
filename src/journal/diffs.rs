e27020928f29264772a43d2f2a8f357dfc2e1189
Aaron Dorrance <celnardur@protonmail.com>
2019-12-23 04:59:42 -0500

    Can construct JournalDiff from git2 diff

--- src/journal/diffs.rs
+++ src/journal/diffs.rs
@@ -1,6 +1,4 @@
 use std::{
-    ops, 
-    collections::HashMap, 
     error::Error, 
     str,
     cell::RefCell,
@@ -21,7 +19,7 @@ pub struct DiffInfo {
 pub fn get_diff_info(info: &mut Vec<DiffInfo>, diff: Diff) -> Result<(), Box<dyn Error>> {
     let info_cell = RefCell::new(info);
 
-    diff.print(git2::DiffFormat::Patch, |delta, hunk, line| {
+    diff.print(git2::DiffFormat::Patch, |delta, _hunk, line| {
         let content = match str::from_utf8(line.content()) {
             Err(_) => return false,
             Ok(s) => s, 
@@ -30,8 +28,8 @@ pub fn get_diff_info(info: &mut Vec<DiffInfo>, diff: Diff) -> Result<(), Box<dyn
         let mut entry = DiffInfo {
             origin: line.origin(),
             content: content.to_string(),
-            new_file: String::from(""),
-            old_file: String::from(""),
+            new_file: String::new(),
+            old_file: String::new(),
         };
 
         match line.origin() {
@@ -55,47 +53,30 @@ pub fn get_diff_info(info: &mut Vec<DiffInfo>, diff: Diff) -> Result<(), Box<dyn
 #[derive(Debug)]
 pub struct JournalDiff {
     pub counts: LineCounts,
-    pub files: HashMap<String, FileChanges>,
+    pub files: Vec<FileChanges>,
 }
 
 
 impl JournalDiff {
-    pub fn new(git_diff: Diff) -> Result<JournalDiff, Box<dyn Error>> {
-        let mut diff = JournalDiff {
+    pub fn new() -> JournalDiff {
+        JournalDiff {
             counts: LineCounts::new(),
-            files: HashMap::new(),
-        };
-        /*
-        let mut file = String::from("None");
-        let mut hunk_index = 0;
+            files: Vec::new(),
+        }
+    }
 
-        // use refcell as a compiler promise to get around closure compiler limitations
-        let diff_cell = RefCell::new(&diff);
+    pub fn construct(&mut self, diff: Diff) -> Result<(), Box<dyn Error>> {
+        let mut info = Vec::new();
+        get_diff_info(&mut info, diff)?;
+        let mut index = 0;
 
-        // couldn't find better interface to get all the data
-        git_diff.print(git2::DiffFormat::Patch, |delta, hunk, line| {
-            let content = match str::from_utf8(line.content()) {
-                Err(_) => return false,
-                Ok(s) => s, 
-            };
-
-            match line.origin() {
-                '+' | '-' | ' ' => print!("{} {}", line.origin(), content),
-                'F' => {
-                    file = match delta.new_file().path_bytes() {
-                        Some(p) => String::from(str::from_utf8(p).unwrap()),
-                        None => String::from("None"),
-                    };
-
-                    let diff = diff_cell.borrow_mut();
-                },
-                'H' => print!("  {}", content),
-                _ => print!("{} {}", line.origin(), content),
-            }
-            true
-        })?;
-        */
-        Ok(diff)
+        while info.get(index).is_some() {
+            let mut changes = FileChanges::new();
+            changes.construct(&info, &mut index);
+            self.counts.add(&changes.counts);
+            self.files.push(changes);
+        }
+        Ok(())
     }
 }
 
@@ -108,11 +89,67 @@ pub struct FileChanges {
     pub hunks: Vec<Hunk>,
 }
 
+impl FileChanges {
+    pub fn new() -> FileChanges {
+        FileChanges {
+            counts: LineCounts::new(),
+            header: String::new(),
+            old_path: String::new(),
+            new_path: String::new(),
+            hunks: Vec::new(),
+        }
+    }
+
+    fn construct(&mut self, info: &Vec<DiffInfo>, index: &mut usize) {
+        if info[*index].origin != 'F' {
+            return;
+        }
+
+        self.header.push_str(&info[*index].content);
+        self.old_path.push_str(&info[*index].old_file);
+        self.new_path.push_str(&info[*index].new_file);
+        *index += 1;
+        while info.get(*index).is_some() && info[*index].origin == 'H' {
+            let hunk = Hunk::new(info, index);
+            self.counts.add(&hunk.counts);
+            self.hunks.push(hunk);
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Hunk {
     pub counts: LineCounts,
     pub header: String,
     pub content: String,
+}
+
+impl Hunk {
+    fn new(info: &Vec<DiffInfo>, index: &mut usize) -> Hunk {
+        let mut hunk = Hunk {
+            counts: LineCounts::new(),
+            header: String::from(&info[*index].content),
+            content: String::new(),
+        };
+
+        *index += 1;
+        let mut origin = info[*index].origin;
+        loop {
+            match origin {
+                '+' => hunk.counts.added += 1,
+                '-' => hunk.counts.deleted += 1,
+                ' ' => (),
+                _ => break,
+            }
+            hunk.content.push_str(&format!("{} {}", origin, info[*index].content));
+            *index += 1;
+            origin = match info.get(*index) {
+                Some(i) => i.origin,
+                None => break,
+            }
+        }
+        hunk
+    }
 }
 
 #[derive(Debug)]
@@ -130,16 +167,11 @@ impl LineCounts {
             modified: 0,
         }
     }
-}
 
-impl ops::Add<LineCounts> for LineCounts {
-    type Output = LineCounts;
-
-    fn add(self, rhs: LineCounts) -> LineCounts {
-        LineCounts {
-            added: self.added + rhs.added,
-            deleted: self.deleted + rhs.deleted,
-            modified: self.modified + rhs.modified,
-        }
+    fn add(&mut self, rhs: &LineCounts) {
+        self.added += rhs.added;
+        self.deleted += rhs.deleted;
+        self.modified += rhs.modified;
     }
 }
+
