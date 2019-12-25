915904b44a396179d968deb0f722d466fab0ce2a
Aaron Dorrance <celnardur@protonmail.com>
2019-12-24 23:43:18 -0500

    Added Error Class

--- src/journal/mod.rs
+++ src/journal/mod.rs
@@ -1,2 +1,28 @@
 pub mod diffs;
 
+use std::error;
+use std::fmt;
+use std::string::String;
+
+#[derive(Debug)]
+pub struct Error {
+    message: String,
+}
+
+impl Error {
+    fn new(message: &str) -> Error {
+        Error { message: String::from(message) }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Journal Error: {}", self.message)
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
+}
